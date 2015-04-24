bee1141e0824c28aac280a65bc4880e5095e25d4
roberto.camacho <roberto.camacho@tum.de>
2015-04-24 11:17:16 -0600

    Execution time script for matrix multiply application

--- PredictionFrameworkPhi/exec_time.py
+++ PredictionFrameworkPhi/exec_time.py
@@ -1,12 +1,54 @@
-import random
 import xml.etree.ElementTree as ET
+import os
+import subprocess
 
+    
 def run():
     xml_file = "execution_data.xml"
-    new_value = random.randint(1, 30)
-    #tree = ET.parse(xml_file)
-    #root = tree.getroot()
-    #for core in root.iter('cores'):
-    #    core.text = str(new_value)
-    #tree.write(xml_file, xml_declaration=True)
-    return new_value
+    env = os.environ.copy()
+    env['SINK_LD_LIBRARY_PATH'] = '/home/rcamacho'
+    (app, affinity, cores, input_size, tpc) = get_variables(xml_file)
+    threads = str(int(cores)*int(tpc))
+    command = build_command(app, affinity, input_size, cores, tpc, threads)
+    outputString = subprocess.check_output(command, env=env, shell=True)
+    decodedString = outputString.decode('UTF-8')
+    execTime = float(decodedString.rsplit(' ', 3)[1])
+    
+    return execTime
+
+def build_command(app, affinity, inputSize, cores, tpc, threads):
+    command = "micnativeloadex "
+    if app.count("matrixMultiply") > 0:
+        command += app
+        command += " -a \""
+        command += inputSize
+        command += " "
+        command += threads
+        command += " 10 10\" -e \"LD_LIBRARY_PATH=/home/rcamacho KMP_PLACE_THREADS="
+        command += cores
+        command += "c,"
+        command += tpc
+        command += "t KMP_AFFINITY="
+        command += affinity
+        command += "\""
+    return command
+    
+    
+def get_variables(xml_file):
+    tree = ET.parse(xml_file)
+    root = tree.getroot()
+    
+    for application in root.iter('application'):
+        app = application.text
+    for affinities in root.iter('affinity'):
+        affinity = affinities.text
+    for core in root.iter('cores'):
+        cores = core.text
+    for inputs in root.iter('input_size'):
+        inputSize = inputs.text
+    for tpcs in root.iter('threads_per_core'):
+        tpc = tpcs.text
+        
+    return app, affinity, cores, inputSize, tpc
+
+run()
